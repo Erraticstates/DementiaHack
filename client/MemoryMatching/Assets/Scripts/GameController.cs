7f85eb9a7207ed7038a6359716d04b9f4f9506b8
Thomas Lai <tlai@fb.com>
2015-11-08 04:39:01 -0500

    Added event logging to client

--- client/MemoryMatching/Assets/Scripts/GameController.cs
+++ client/MemoryMatching/Assets/Scripts/GameController.cs
@@ -2,51 +2,56 @@
 using System.Collections;
 
 public class GameController : MonoBehaviour {
+	public const string URL_BASE = "http://prod-spbe7sxihm.elasticbeanstalk.com/index.php";
+	public const string PARSE_BASE = "http://inyour-w9my2y2jii.elasticbeanstalk.com/index.php";
+	public const string UID = "uid";
+	public const string GAME_ID = "game_id";
+	public const string SESSION_ID = "session_id";
+	public const string EVENT_TIME = "event_time";
+	public const string EVENT_DATA = "event_data";
+
 	public string[] patternTypes;
 	public Box[] boxGrid = new Box[16];
 	public Box lastBoxClicked;
 	public Box secondLastBoxClicked;
 	public int skipFrames;
 	private int numSolved;
+	private EventLogger eventLogger;
 
 	public class Box {
 		GameController gameController;
 		Vector3 position;
 		public string patternName;
 		private GameObject cover;
-		private bool wasOpened;
 		private bool isCovered;
-
-		public Box() {
-		}
 
 		public Box(GameController controller, Vector3 position, string patternName) {
 			this.gameController = controller;
 			this.patternName = patternName;
+			this.position = position;
 			print ("Box instantiated with pattern = " + patternName);
 			Instantiate(Resources.Load ("Prefabs/" + patternName), position, Quaternion.identity);
 			Vector3 coverPosition = new Vector3(position.x, position.y, -1);
 			print ("Position = " + coverPosition.x + ", " + coverPosition.y + ", " + coverPosition.z);
 			cover = Instantiate (Resources.Load ("Prefabs/Cover"), coverPosition, Quaternion.identity) as GameObject;
-			wasOpened = false;
 			isCovered = true;
 			BoxController boxController = cover.GetComponent<BoxController>();
 			boxController.setParentBox(this);
 		}
 
 		public void onClick() {
-			print ("onClick");
+			print ("clicked on " + position.x + ", " + position.y);
 			if (gameController.skipFrames > 0 
 			    || !cover.activeInHierarchy 
 			    || (gameController.lastBoxClicked != null && gameController.secondLastBoxClicked != null)) {
 				return;
 			}
-			wasOpened = true;
 			isCovered = false;
 			if (gameController.lastBoxClicked != null) {
 				gameController.secondLastBoxClicked = gameController.lastBoxClicked;
 				gameController.skipFrames = 60;
 			}
+			gameController.eventLogger.addEvent (gameController.calculatePosFromVector (position));
 			gameController.lastBoxClicked = this;
 		}
 
@@ -65,23 +70,33 @@ public class GameController : MonoBehaviour {
 		winText.transform.localScale = new Vector3(0f, 0f, 0f);
 		numSolved = 0;
 		patternTypes = new string[] {
-			"arrow-block-rotated", 
-			"box-full",
-			"diamond-outline",
-			"frame-small",
-			"gear-hole",
-			"next-full",
-			"page-overlaid",
-			"star-outline",
+			"arrow-block-rotated", // 0
+			"box-full", // 1
+			"diamond-outline", // 2
+			"frame-small", // 3
+			"gear-hole", // 4
+			"next-full", // 5
+			"page-overlaid", // 6
+			"star-outline", // 7
 		};
 		Vector3[] positions = new Vector3[16];
+		int[] patterns = new int[16];
 		for (int i=0; i < 16; i++) {
 			positions[i] = new Vector3((i%4)*2 - 3, (i/4)*2 - 3, 0);
 		}
 		positions = randomizePos (positions);
 		for (int i=0; i < 16; i++) {
 			boxGrid[i] = new Box(this, positions[i], patternTypes[i/2]);
+			patterns[calculatePosFromVector(positions[i])] = i/2;
 		}
+
+		eventLogger = new EventLogger ();
+		eventLogger.setup (this);
+		eventLogger.registerPattern (patterns);
+	}
+
+	public int calculatePosFromVector(Vector3 vec) {
+		return (int)(vec.x + 3) / 2 + (int)(vec.y + 3) * 2;
 	}
 
 	Vector3[] randomizePos(Vector3[] positions) {
@@ -99,6 +114,7 @@ public class GameController : MonoBehaviour {
 		if (numSolved >= 8) {
 			GameObject winText = GameObject.FindWithTag ("WinTag");
 			winText.transform.localScale = new Vector3(0.14f, 0.14f, 1f);
+			eventLogger.publish();
 			numSolved = 0;
 		}
 		// Update every box
@@ -108,7 +124,7 @@ public class GameController : MonoBehaviour {
 
 		// Check for 2 clicks
 		if (skipFrames > 0) {
-			print("skipFrames = " + skipFrames);
+//			print("skipFrames = " + skipFrames);
 			skipFrames--;
 		} else if (lastBoxClicked != null && secondLastBoxClicked != null) {
 			if (!lastBoxClicked.patternName.Equals(secondLastBoxClicked.patternName)) {
@@ -122,6 +138,77 @@ public class GameController : MonoBehaviour {
 			}
 			lastBoxClicked = null;
 			secondLastBoxClicked = null;
+		}
+	}
+
+	public void sendRequest(WWW www) {
+		StartCoroutine (WaitForRequest (www));
+	}
+
+	public IEnumerator WaitForRequest(WWW www) {
+		yield return www;
+		
+		if (www.error == null) {
+			print ("WWW OK! " + www.text);
+		} else {
+			print ("WWW ERROR! " + www.text);
+		}
+	}
+	
+	public class EventLogger {
+		public const string URL_BASE = "http://prod-spbe7sxihm.elasticbeanstalk.com/index.php";
+		public const string UID = "uid";
+		public const string GAME_ID = "game_id";
+		public const string SESSION_ID = "session_id";
+		public const string EVENT_TIME = "event_time";
+		public const string EVENT_DATA = "event_data";
+		public const string PATTERN = "pattern";
+		public const string EVENTS = "events";
+		public const string POSITION = "pos";
+		public const string TIME = "time";
+		private GameController gameController;
+		System.DateTime epochStart;
+		double cur_time;
+		JSONObject eventDataJSON;
+		JSONObject eventsArrayJSON;
+		WWWForm form;
+
+		public void setup(GameController gameController) {
+			this.gameController = gameController;
+			epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+			cur_time = (System.DateTime.UtcNow - epochStart).TotalSeconds;
+			eventDataJSON = new JSONObject (JSONObject.Type.OBJECT);
+			eventsArrayJSON = new JSONObject (JSONObject.Type.ARRAY);
+			eventDataJSON.AddField (EVENTS, eventsArrayJSON);
+		}
+
+		public void registerPattern(int[] patterns) {
+			JSONObject arr = new JSONObject (JSONObject.Type.ARRAY);
+			for (int i=0; i<patterns.Length; i++) {
+				arr.Add (patterns [i]);
+			}
+			eventDataJSON.AddField (PATTERN, arr);
+			print ("Patterns = " + eventDataJSON.Print ());
+		}
+
+		public void addEvent(int pos) {
+			JSONObject eventJson = new JSONObject (JSONObject.Type.OBJECT);
+			eventJson.AddField (POSITION, pos);
+			eventJson.AddField(TIME, (System.DateTime.UtcNow - epochStart).TotalSeconds.ToString());
+			eventsArrayJSON.Add (eventJson);
+			print ("Event = " + eventJson.Print());
+		}
+
+		public void publish() {
+			WWWForm form = new WWWForm ();
+			form.AddField (UID, "1");
+			form.AddField (GAME_ID, "1");
+			form.AddField (SESSION_ID, (int) (Random.value*10000f));
+			form.AddField (EVENT_TIME, cur_time.ToString());
+			form.AddField (EVENT_DATA, eventDataJSON.Print());
+			print ("Final JSON = " + eventDataJSON.Print ());
+			WWW www = new WWW (PARSE_BASE, form);
+			gameController.sendRequest (www);
 		}
 	}
 }
